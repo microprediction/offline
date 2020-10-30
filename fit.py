c984610029b3ef64e81b30d003eb5b27fdecf2fb
Peter Cotton <peter.cotton@microprediction.com>
2020-10-29 22:32:51 -0400

    dependencies added

--- fit.py
+++ fit.py
@@ -1,19 +1,21 @@
 from microprediction import MicroReader
 from statsmodels.tsa.api import SimpleExpSmoothing
 import os
-MODEL_FIT = os.path.dirname(os.path.realpath(__file__))+os.path.sep+'modelfits'
+import random
+MODEL_FIT_PATH = os.path.dirname(os.path.realpath(__file__)) + os.path.sep + 'modelfits'
 
-STREAMS = ['electricity-load-nyiso-overall.json']
+mr = MicroReader()
+STREAMS = mr.get_stream_names()
 
 
-def fit_all():
-    mr = MicroReader()
-    for name in STREAMS:
-        lagged_values = list(reversed(mr.get_lagged_values(name=name, count=5000)))
-        if len(lagged_values)>300:
-            model_fit = SimpleExpSmoothing(lagged_values, initialization_method="estimated").fit()
-            model_fit.save(MODEL_FIT+os.path.sep+name.replace('.json','.pkl'))
+def fit_one():
+    """ Pick a random stream and update parameters """
+    name = random.choice(STREAMS)
+    lagged_values = list(reversed(mr.get_lagged_values(name=name, count=5000)))
+    if len(lagged_values)>300:
+        model_fit = SimpleExpSmoothing(lagged_values, initialization_method="estimated").fit()
+        model_fit.save(MODEL_FIT_PATH + os.path.sep + name.replace('.json', '.pkl'))
 
 
 if __name__=='__main__':
-    fit_all()
+    fit_one()
