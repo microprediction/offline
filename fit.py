a7c8b8b815a5b5ec6dcd3a096a832ff2c901b585
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2020-12-16 19:10:04 -0500

    Update fit.py

--- fit.py
+++ fit.py
@@ -4,5 +4,6 @@ from fit_expsmooth import fit_one as fit_one_expsmooth
 
 if __name__=='__main__':
     fit_one_expsmooth()
-    fit_one_expnorm()
+    for _ in range(5):
+        fit_one_expnorm()
 
